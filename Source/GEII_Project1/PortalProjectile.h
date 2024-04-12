3ddf0b254ed196b324088e5f06530cfc1e5845ba
Catarina-SE <2222141@my.ipleiria.pt>
2024-04-12 18:18:30 +0100

    First Changes

---
+++ Source/GEII_Project1/PortalProjectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GEII_Project1Projectile.h"
+#include "PortalProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEII_PROJECT1_API APortalProjectile : public AGEII_Project1Projectile
+{
+	GENERATED_BODY()
+	
+};
